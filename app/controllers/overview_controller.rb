1f87b913465a23555bfec0b0c00029a5defb7b57
Jiří Beneš <benes@nandu.cz>
2011-09-07 22:08:42 +0200

    Added week by week calculations

--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -8,8 +8,18 @@ class OverviewController < ApplicationController
     @current_month_incomes = Income.current_month.all
     @current_month_expenses_amount = Expense.current_month.sum(:price)
     @current_month_incomes_amount = Income.current_month.sum(:amount)
+    @savings = Saving.first.amount
     
-    @savings = Saving.all.first.amount
+    number_of_weeks = Time.days_in_month(Date.today.month).to_f / 7
+    
+    weekly_to_spend = (@fixed_incomes_amount - @current_month_fixed_expenses_amount - @savings) / number_of_weeks
+    
+    @weekly_balance = Array.new
+    number_of_weeks.to_i.times do |i|
+      current_week_expenses = Expense.all_for_week(i).sum(:price)
+      @weekly_balance << weekly_to_spend - current_week_expenses
+    end
+    
     @balance = @fixed_incomes_amount + @current_month_incomes_amount - (@current_month_expenses_amount + @current_month_fixed_expenses_amount) - @savings
   end
 end
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -6,5 +6,13 @@ class Expense < ActiveRecord::Base
     {:conditions => ["created_at >= ?",
       Date.today.beginning_of_month]}
   }
+  named_scope :all_for_week, lambda { |i|
+    i = i+1
+    puts Date.new(Time.now.year,Time.now.month,((i*7)-6))
+    beginning_of_week = Date.new(Time.now.year,Time.now.month,((i*7)-6))
+    end_of_week = Date.new(Time.now.year,Time.now.month,(i*7))
+    {:conditions => ["created_at >= ? AND created_at <= ?",
+      beginning_of_week, end_of_week]}
+  }
 
 end
